7eb7026fcd7dcf7e5051ab3077132c419794b6fa
kama3547 <kamilmelisov937@gmail.com>
2021-09-08 19:19:04 +0600

    Сделал доп

---
+++ app/src/main/java/com/example/kotlin1/ui/activity/ActivitySecond.kt
@@ -0,0 +1,34 @@
+package com.example.kotlin1.ui.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.kotlin1.ui.activity.MainActivity.Companion.GET_TEXT
+import com.example.kotlin1.databinding.ActivitySecondBinding
+
+class ActivitySecond : AppCompatActivity() {
+
+    lateinit var binding : ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txtT.text = intent.getStringExtra(GET_TEXT).toString()
+        binding.btn2.setOnClickListener {
+            if (binding.editTxt2.text.trim().toString().isEmpty()
+            ) {
+                Toast.makeText(this, "EditText не может быть пустым",Toast.LENGTH_SHORT).show()
+            }else{
+                startActivity(Intent(this, MainActivity::class.java)
+                    .putExtra(GET_TEXT2,binding.editTxt2.text.toString()))
+            }
+        }
+    }
+
+    companion object{
+        const val GET_TEXT2 = "data2"
+    }
+    }
---
+++ app/src/main/java/com/example/kotlin1/ui/activity/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.kotlin1.ui.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.kotlin1.ui.activity.ActivitySecond.Companion.GET_TEXT2
+import com.example.kotlin1.databinding.ActivityMainBinding
+import com.example.kotlin1.databinding.ActivitySecondBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+            binding.txt2.text = intent.getStringExtra(GET_TEXT2) ?: ""
+            binding.btnB.setOnClickListener {
+                if (binding.editTxt.text.trim().toString().isEmpty()
+                ) {
+                    Toast.makeText(this, "EditText не может быть пустым",Toast.LENGTH_SHORT).show()
+                } else {
+                    startActivity(
+                        Intent(this, ActivitySecond::class.java).putExtra(
+                            GET_TEXT,
+                            binding.editTxt.text.toString()
+                        )
+                    )
+                }
+            }
+
+        }
+
+    companion object {
+        const val GET_TEXT = "data"
+    }
+}
